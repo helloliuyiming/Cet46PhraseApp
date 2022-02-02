c34be8eb11f2fc0d7f586997fd3982dbb107a795
lym <gliuyiming@gmail.com>
2022-02-02 23:39:33 +0800

    为初始数据提供加密

--- app/src/main/java/com/lixiangya/cet46phrase/MyApplication.kt
+++ app/src/main/java/com/lixiangya/cet46phrase/MyApplication.kt
@@ -1,23 +1,39 @@
 package com.lixiangya.cet46phrase
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.pm.PackageManager
+import android.content.pm.Signature
+import android.os.Build
 import android.util.Base64
 import android.util.Log
-import com.google.android.gms.ads.AdRequest
+import android.widget.Toast
+import cn.hutool.crypto.digest.MD5
 import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
 import com.google.gson.Gson
 import com.lixiangya.cet46phrase.util.PreferencesUtil
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import java.lang.StringBuilder
+import java.security.MessageDigest
 import java.security.SecureRandom
 import java.util.*
+import kotlin.math.log
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+
+        val key = "secretKey"
+        val sharePreferences = PreferencesUtil.getSharePreferences(this)
+        if (sharePreferences.getString(key, null) == null) {
+            val loadKey = loadKey()
+            val edit = sharePreferences.edit()
+            edit.putString(key,loadKey)
+            edit.apply()
+        }
 
         initAdMod()
         val preference = PreferencesUtil.getSharePreferences(this)
@@ -37,6 +53,41 @@ class MyApplication : Application() {
             .encryptionKey(Base64.decode(dbKeyStr!!,Base64.DEFAULT))
             .build()
         Realm.setDefaultConfiguration(realmConfig)
+
+        val packageManager = this.packageManager
+        val packageName = packageName
+        Log.i("main","packageName:$packageName")
+        val signature: Signature
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            if (packageInfo?.signingInfo == null) {
+                throw Exception("应用签名信息错误")
+            }
+            signature = packageInfo.signingInfo.apkContentsSigners[0]
+        }else{
+            @SuppressLint("PackageManagerGetSignatures")
+            val packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            signature = packageInfo.signatures[0]
+        }
+        val signatureByteArray = signature.toByteArray()
+        Log.i("main","signaturePlainText = ${String(signatureByteArray)}")
+        val mD5 = MD5.create()
+        val digestHex16 = mD5.digestHex16(signatureByteArray)
+        Log.i("main","app signature md5 = $digestHex16")
+        Toast.makeText(this,"app signature md5 = $digestHex16",Toast.LENGTH_LONG).show()
+
+        val md: MessageDigest = MessageDigest.getInstance("SHA1")
+        val publicKey: ByteArray = md.digest(signatureByteArray)
+        val hexString = StringBuilder()
+        for (i in publicKey.indices) {
+            val appendString = Integer.toHexString(0xFF and publicKey[i].toInt()).uppercase(Locale.US)
+            if (appendString.length == 1) hexString.append("0")
+            hexString.append(appendString)
+            hexString.append(":")
+        }
+        Log.i("main","app signature sha1 = $hexString")
     }
 
     private fun initAdMod() {
@@ -50,6 +101,14 @@ class MyApplication : Application() {
         MobileAds.initialize(this){
             Log.d("main", "initAdsense() called")
             Log.i("main", Gson().toJson(it.adapterStatusMap))
+        }
+    }
+
+    external fun loadKey():String
+
+    companion object{
+        init {
+            System.loadLibrary("cet46phrase");
         }
     }
 }
--- app/src/main/java/com/lixiangya/cet46phrase/viewmodel/FragmentLoadDataViewModel.kt
+++ app/src/main/java/com/lixiangya/cet46phrase/viewmodel/FragmentLoadDataViewModel.kt
@@ -1,10 +1,13 @@
 package com.lixiangya.cet46phrase.viewmodel
 
 import android.app.Application
+import android.util.Base64
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import cn.hutool.crypto.symmetric.AES
 import com.lixiangya.cet46phrase.entity.Phrase
 import com.google.gson.Gson
+import com.lixiangya.cet46phrase.util.PreferencesUtil
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableOnSubscribe
@@ -21,12 +24,16 @@ class FragmentLoadDataViewModel(application: Application) : AndroidViewModel(app
     fun load(): Observable<Int> {
         return Observable.create<Int> {
             val realm = Realm.getDefaultInstance()
-            val inputStream = context.assets.open("phrases.json")
+            val sharePreferences = PreferencesUtil.getSharePreferences(context)
+            val secretKey = sharePreferences.getString("secretKey", null)
+            val inputStream = context.assets.open("DejNBqfXGyGaDGYl")
             val isr = InputStreamReader(inputStream)
+            val aes = AES(Base64.decode(secretKey,Base64.DEFAULT))
             realm.beginTransaction()
             isr.readLines().forEachIndexed { index, line ->
                 count++
-                val phrase = gson.fromJson(line, Phrase::class.java)
+                val plainText = String(aes.decrypt(line))
+                val phrase = gson.fromJson(plainText, Phrase::class.java)
                 phrase.id = index
                 realm.insertOrUpdate(phrase)
                 it.onNext(index)
